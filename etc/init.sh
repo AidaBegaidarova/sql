034a9aff3a80dfdcbf33daa695cc60661177ebde
magnitius <zak4ever@mail.ru>
2017-09-24 16:01:48 +0800

    another update

--- etc/init.sh
+++ etc/init.sh
@@ -4,5 +4,6 @@ sudo rm /etc/nginx/sites-available/default
 echo files deleted
 sudo /etc/init.d/nginx stop
 sudo ln -s /home/box/web/etc/ngix.conf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/ngix.conf /etc/nginx/sites-available/default
 echo  "Configuration done"
 sudo /etc/init.d/nginx start
