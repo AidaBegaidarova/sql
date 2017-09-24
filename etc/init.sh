94a88a72644fb799af535243d6bf6d5f691f3cf9
magnitius <zak4ever@mail.ru>
2017-09-24 14:03:17 +0800

    Second push

--- etc/init.sh
+++ etc/init.sh
@@ -2,5 +2,5 @@
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo rm -f /etc/nginx/sites-available/default
 
-sudo ln -sf /home/box/web/etc/ngix.conf /etc/nginx/site-enabled/default
+sudo ln -sf /home/box/web/etc/ngix.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
