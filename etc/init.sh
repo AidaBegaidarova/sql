b3f965fa49491d75b0b8845f00d5a55718930168
Anton Voronov <zak4ever@mail.ru>
2017-09-24 21:00:16 +0800

    stepic unicorn

--- etc/init.sh
+++ etc/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo /etc/init.d/nginx stop
+sudo unlink /etc/nginx/sitex-enabled/default
 sudo ln -s /home/box/web/etc/ngix.conf /etc/nginx/sites-enabled/default
 echo  "Configuration done"
-sudo /etc/init.d/nginx start
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+echo "Unicorn done"
+sudo /etc/init.d/gunicorn restart
+
---
+++ hello.py
@@ -0,0 +1,9 @@
+def wsgi_application(environ, start_response):
+    body = ""
+    data = environ["QUERY_STRING"].split('&')
+    for item in data:
+        body += item + '\r\n'
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    start_response(status, headers)
+    return [bytes(body, 'utf-8')]
