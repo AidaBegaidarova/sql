f7ec9c78b3277eeb296f2b136616179a6c1e226c
Anton Voronov <zak4ever@mail.ru>
2017-09-25 09:04:23 +0800

    stepic unicorn

--- etc/init.sh
+++ etc/init.sh
@@ -1,5 +1,5 @@
-sudo unlink /etc/nginx/sitex-enabled/default
-sudo ln -sf /home/box/web/etc/ngix.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sitex-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 echo  Configuration done
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
