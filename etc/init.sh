0847bc4fb6fbbcb892ae5dec84f247634303cd33
Anton Voronov <zak4ever@mail.ru>
2017-09-25 09:01:58 +0800

    stepic unicorn

--- etc/init.sh
+++ etc/init.sh
@@ -2,7 +2,7 @@ sudo unlink /etc/nginx/sitex-enabled/default
 sudo ln -sf /home/box/web/etc/ngix.conf /etc/nginx/sites-enabled/default
 echo  Configuration done
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 echo "Unicorn done"
 sudo /etc/init.d/gunicorn restart
 
