da8c5b18200b800e871f0ba7703f15624f3640c7
Anton Voronov <zak4ever@mail.ru>
2017-09-25 08:59:29 +0800

    stepic unicorn

--- etc/hello.py
+++
@@ -1,5 +0,0 @@
-bind = "0.0.0.0:8080"
-workers = 3
-logfile = "var/log/gunicorn.log"
-loglevel = "info"
-proc_name = "hello"
--- etc/init.sh
+++ etc/init.sh
@@ -1,8 +1,8 @@
 sudo unlink /etc/nginx/sitex-enabled/default
-sudo ln -s /home/box/web/etc/ngix.conf /etc/nginx/sites-enabled/default
-echo  "Configuration done"
+sudo ln -sf /home/box/web/etc/ngix.conf /etc/nginx/sites-enabled/default
+echo  Configuration done
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 echo "Unicorn done"
 sudo /etc/init.d/gunicorn restart
 
