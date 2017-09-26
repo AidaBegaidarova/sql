a66b76e9b68d3d4a6f386a9bbd93a7a07ee17a7b
Anton Voronov <zak4ever@mail.ru>
2017-09-26 12:35:57 +0800

    stepic unicorn

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,7 +14,7 @@ class QuestionManager(models.Manager):
 
 class Question(models.Model):
     object = QuestionManager()
-    tittle = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
